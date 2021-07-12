be955c318a97c02e5341a26d4d0988e4f7f702ff
KunChi232 <kuo23201199@gmail.com>
2021-07-12 10:35:46 +0800

    Add TrainEpoch class

--- util/Epoch.py
+++ util/Epoch.py
@@ -1,21 +1,28 @@
+import torch
+import torch.nn.functional as F
+
 import tqdm.tqdm as tqdm
-
 from meter import AverageValueMeter
-
 import sys
 
 class Epoch:
-    def __init__(self, model, loss, device = 'cuda', stage = 'Train'):
+    def __init__(self, model, loss, device = 'cuda', stage = 'Train', optimizer = None, positive_count = 0, negative_count = 0):
         self.model = model
         self.loss = loss
         self.device = device
         self.stage = stage
-
+        self.optimizer = optimizer
         self.to_device()
+
+        self.positive_count = positive_count
+        self.negative_count = negative_count
+        
 
     def to_device(self):
         self.model.to(self.device)
         self.loss.to(self.device)
+        if(self.optimizer):
+            self.optimizer.to(self.device)
 
     def on_epoch_start(self):
         pass
@@ -34,7 +41,7 @@ class Epoch:
                 x = [xi.to(self.device) for xi in x]
                 y = y.to(self.device)
                 
-                loss = self.batch_update()
+                loss, pred, weights = self.batch_update(x, y)
                 loss = loss.cpu().detach().numpy()
                 loss_meter.add(loss)
                 logs.update({'loss' : loss})
@@ -42,3 +49,22 @@ class Epoch:
                 iterator.set_postfix_str('Loss:'+str(logs['loss']))
 
         return logs
+
+
+class TrainEpoch(Epoch):
+    def __init__(self):
+        super().__init__()
+
+    def on_epoch_start(self):
+        self.model.train()
+
+    def batch_update(self, x, y):
+        self.optimizer.zero_grad()
+        pred, instance_loss, cluster_attention_weight = self.model(x, y)
+        bag_loss = F.cross_entropy(pred, y, weight = torch.tensor([1/self.negative_count, 1/self.positive_count]))
+        total_loss = bag_loss + instance_loss
+
+        total_loss.backward()
+        self.optimizer.step()
+
+        return total_loss, pred, cluster_attention_weight
