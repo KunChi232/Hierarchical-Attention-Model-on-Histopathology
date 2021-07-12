43e1ffd99158b212158b1a9a7d9c165b16f1ef37
KunChi232 <kuo23201199@gmail.com>
2021-07-12 10:45:44 +0800

    Add ValidEpoch class

--- util/Epoch.py
+++ util/Epoch.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn.functional as F
 
-import tqdm.tqdm as tqdm
+from tqdm import tqdm
 from meter import AverageValueMeter
 import sys
 
+from sklearn.metrics import roc_auc_score, average_precision_score
 class Epoch:
     def __init__(self, model, loss, device = 'cuda', stage = 'Train', optimizer = None, positive_count = 0, negative_count = 0):
         self.model = model
@@ -68,3 +69,57 @@ class TrainEpoch(Epoch):
         self.optimizer.step()
 
         return total_loss, pred, cluster_attention_weight
+
+
+class ValidEpoch(Epoch):
+    def __init__(self):
+        super().__init__()
+    
+    def on_epoch_start(self):
+        self.model.eval()
+
+    def batch_update(self, x, y):
+        with torch.no_grad():
+            pred, instance_loss, cluster_attention_weight = self.model(x, y)
+            bag_loss = F.cross_entropy(pred, y, weight = torch.tensor([1/self.negative_count, 1/self.positive_count]))
+            total_loss = bag_loss + instance_loss
+
+        return total_loss, pred, cluster_attention_weight
+
+
+    def run(self, dataloder):
+        self.on_epoch_start()
+
+        loss_meter = AverageValueMeter()
+        logs = {}
+
+        ground_truth = []
+        model_prediction = []
+
+        with tqdm(dataloder, desc = self.stage, file = sys.stdout) as iterator:
+            for x, y, patch_names in iterator:
+                x = [xi.to(self.device) for xi in x]
+                y = y.to(self.device)
+                
+                loss, pred, weights = self.batch_update(x, y)
+                loss = loss.cpu().detach().numpy()
+                loss_meter.add(loss)
+                logs.update({'loss' : loss})
+
+                iterator.set_postfix_str('Loss:'+str(logs['loss']))
+
+                #store each bag label and prediction
+                y = y.detach().cpu().numpy()
+                output = torch.softmax(output, dim = 1)[:, 1].detach().cpu().numpy()
+                for p, yi in zip(output, y):
+                    model_prediction.append(p)
+                    ground_truth.append(yi)
+
+        auroc = roc_auc_score(ground_truth, model_prediction)
+        aupr = average_precision_score(ground_truth, model_prediction)
+
+        print('AUC: %0.2f, AUPR: %0.2f' %(auroc, aupr))
+
+        logs.update({'AUC':aupr, 'AUPR':aupr})
+
+        return logs
